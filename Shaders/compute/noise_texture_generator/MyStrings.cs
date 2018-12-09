44d989cb1034bbf3a63edb5f2dce87e000a94cdc
Marek Kowalik <mkowalik92@gmail.com>
2018-12-09 17:19:39 -0500

    Create MyStrings.cs

---
+++ Shaders/compute/noise_texture_generator/MyStrings.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MyStrings
+{
+    public const string ComputePractice = "ComputePractice";
+    public const string FillWithRed = "FillWithRed";
+    public const string result = "result";
+
+    public const string WavePractice = "WavePractice";
+    public const string FillWithRandom = "FillWithRandom";
+    public const string outputTexture = "outputTexture";
+}
