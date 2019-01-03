5633bbad597dba5ed60d528078bacfa978b2e18e
Marek Kowalik <mkowalik92@gmail.com>
2019-01-03 15:45:16 -0500

    Add files via upload

---
+++ src/scripts/shaders/FFT/ConstStrings.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public static class ConstStrings
+{
+    /* Shader const strings */
+    public const string shaderMainProgramstr = "main";
+    // h0k
+    public const string noiseTextureStr1 = "noiseTexture1";
+    public const string noiseTextureStr2 = "noiseTexture2";
+    public const string noiseTextureStr3 = "noiseTexture3";
+    public const string noiseTextureStr4 = "noiseTexture4";
+
+    public const string shaderh0kOutputTextureStr = "h0kTexture";
+    public const string shaderh0minuskOutputTextureStr = "h0minuskTexture";
+    // twiddle factors
+    public const string shaderTwiddleFactorsOutputTextureStr = "twiddleFactorsTexture";
+
+    public const string shaderTwiddleFactorsInput_bitReverseIndices = "bitReversedIndices";
+
+    // ocean settings
+    public const string shaderInput_N = "N";
+    public const string shaderInput_L = "L";
+    public const string shaderInput_amplitude = "amplitude";
+    public const string shaderInput_windSpeed = "windSpeed";
+    public const string shaderInput_capillarSupressFactor = "capillarSupressFactor";
+    public const string shaderInput_direction = "direction";
+}
---
+++ src/scripts/shaders/FFT/TwiddleFactors.cs
@@ -0,0 +1,60 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class TwiddleFactors
+{
+    private int N; // Ocean mesh plane vertex resolution
+
+    private ComputeShader twiddleFactorsComputeShader;
+    private int kernelProgramId; // Shader kernel program ID
+
+    public RenderTexture twiddleFactorsOutputRenderTexture;
+
+    int log_2_N;
+
+    public TwiddleFactors (int N, ComputeShader twiddleFactorsComputeShader)
+    {
+        this.N = N;
+        this.twiddleFactorsComputeShader = twiddleFactorsComputeShader;
+
+        log_2_N = (int)(Mathf.Log(N) / Mathf.Log(2));
+    }
+
+    public void Run ()
+    {
+        // Create twiddle factors render texture output
+        twiddleFactorsOutputRenderTexture = new RenderTexture(log_2_N, N, 0);
+        // Enable writing to twiddle factors output render texture
+        twiddleFactorsOutputRenderTexture.enableRandomWrite = true;
+        // "Actually create render texture"
+        twiddleFactorsOutputRenderTexture.Create();
+
+        // Get shader kernel id of program we want to execute
+        kernelProgramId = twiddleFactorsComputeShader.FindKernel(ConstStrings.shaderMainProgramstr);
+        // Set output textures
+        twiddleFactorsComputeShader.SetTexture(kernelProgramId, ConstStrings.shaderTwiddleFactorsOutputTextureStr, twiddleFactorsOutputRenderTexture);
+
+        // Initialize bit reversed indices
+        // Declare int array of size N (mesh vertex resolution n x n = N ) | Currently supports 256x256
+        int[] bitReversedIndices = new int[N];
+        for (int i = 0; i < N; i++)
+        {
+            string indexBinaryString = System.Convert.ToString(i, 2).PadLeft(log_2_N, '0');
+            bitReversedIndices[i] = System.Convert.ToInt32(Reverse(indexBinaryString), 2);
+        }
+        // Set bit reversed indices(indices int array bitReversedIndices)
+        twiddleFactorsComputeShader.SetInts(ConstStrings.shaderTwiddleFactorsInput_bitReverseIndices, bitReversedIndices);
+        // Set input N uniform value
+        twiddleFactorsComputeShader.SetInt(ConstStrings.shaderInput_N, N);
+
+        //// Run twiddle factors compute shader
+        twiddleFactorsComputeShader.Dispatch(kernelProgramId, log_2_N, N / 16, 1);
+    }
+    private static string Reverse(string indexString)
+    {
+        char[] indexCharArray = indexString.ToCharArray();
+        System.Array.Reverse(indexCharArray);
+        return new string(indexCharArray);
+    }
+}
